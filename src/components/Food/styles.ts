3b6085b0a1da0b4932d13c9400053a45fa6357b7
xthiagosant <uzmaki_thiago@outlook.com>
2021-04-02 06:14:03 -0300

    Refatorado o app e o compoente food

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+type THeaderProps = {
+  available: boolean;
+}
+
+export const Container = styled.div<THeaderProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
